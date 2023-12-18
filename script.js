e7f55e0bbc5be9fa0f2121c09c388f584e92f26b
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-18 23:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
 }
 
+
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l= prompt("Enter a number.");
 alert(makeid(l));
